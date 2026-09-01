555362081bab4561cd9b7f18a352e5b5b0b9f328
mahmunirmoon <DRMahMonir@proton.me>
2026-09-01 09:44:55 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./new-modes.css";
 import App from "./App.tsx";
-import { AboutCreditFix } from "./components/AboutCreditFix";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <AboutCreditFix />
   </React.StrictMode>,
 );
